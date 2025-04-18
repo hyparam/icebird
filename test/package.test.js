de5e20eaf73e6e5c908a5b599e34e2fac4e5b7da
Kenny Daniel <platypii@gmail.com>
2025-04-17 23:44:45 -0700

    Publish v0.1.15

--- test/package.test.js
+++ test/package.test.js
@@ -5,12 +5,15 @@ describe('package.json', () => {
   it('should have the correct name', () => {
     expect(packageJson.name).toBe('icebird')
   })
+
   it('should have a valid version', () => {
     expect(packageJson.version).toMatch(/^\d+\.\d+\.\d+$/)
   })
+
   it('should have MIT license', () => {
     expect(packageJson.license).toBe('MIT')
   })
+
   it('should have at most 2 dependencies', () => {
     const { dependencies } = packageJson
     expect(Object.keys(dependencies)).toEqual([
@@ -18,6 +21,7 @@ describe('package.json', () => {
       'hyparquet-compressors',
     ])
   })
+
   it('should have precise dependency versions', () => {
     const { dependencies, devDependencies } = packageJson
     const allDependencies = { ...dependencies, ...devDependencies }
@@ -25,7 +29,20 @@ describe('package.json', () => {
       expect(version).toMatch(/^\d+\.\d+\.\d+$/)
     })
   })
+
   it('should have no peer dependencies', () => {
     expect('peerDependencies' in packageJson).toBe(false)
+  })
+
+  it('should have exports with types first', () => {
+    const { exports } = packageJson
+    expect(exports).toBeDefined()
+    for (const [, exportObj] of Object.entries(exports)) {
+      if (typeof exportObj === 'object') {
+        expect(Object.keys(exportObj)).toEqual(['types', 'import'])
+      } else {
+        expect(typeof exportObj).toBe('string')
+      }
+    }
   })
 })
